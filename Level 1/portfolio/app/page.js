c77bc827a563e0ed458f4f87e724676f47e3b702
Om Kumar <om2005kumar001@gmail.com>
2024-07-18 23:56:50 +0530

    Portfolio

--- Level 1/portfolio/app/page.js
+++ Level 1/portfolio/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import LanguageProficiency from '../components/LanguageProficiency';
 
 export default function Home() {
   return (
@@ -59,9 +60,212 @@ export default function Home() {
           </div>
         </section>
       </div>
-    <div className="about">
-      About
-    </div>
+      <div className="about" id="about">
+        <div className=" bg-slate-200 body-font flex">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="text-center mb-20">
+              <h1 className="sm:text-10xl text-3xl font-medium text-center title-font mb-4">About me</h1>
+              <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto"> Hi there!  I'm Om Kumar, an aspiring Software Engineer currently pursuing a Bachelor of Technology degree in Computer Science Engineering from Asansol Engineering College, affiliated with Maulana Abul Kalam Azad University of Technology. I am a tech enthusiast and a skilled web developer.</p>
+            </div>
+          </div>
+          <div className="container px-5 mx-auto">
+            <Image src="/img/Portarit.jpg" alt="Photo" width={300} height={350} />
+          </div>
+        </div>
+      </div>
+      <div className="Skill bg-slate-200" id="skill" >
+        <div className="container mx-auto">
+          <main className="flex flex-col items-center justify-center min-h-screen py-16">
+            <h1 className="text-5xl font-bold mb-8">Skills</h1>
+            <LanguageProficiency />
+          </main>
+        </div>
+      </div>
+      <div className="Projects" id="Projects">
+        <section className="text-black bg-slate-200 body-font">
+          <h1 className="sm:text-6xl text-3xl font-medium text-center title-font text-black mb-4">My Projects</h1>
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              <div className="p-4 md:w-1/3">
+                <div className="h-full border-2 border-slate-300 rounded-lg overflow-hidden">
+                  <Image
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src="/img/Screenshot 2024-04-10 154642.png"
+                    alt="Twitter Clone"
+                    width={500}
+                    height={300}
+                  />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Clone</h2>
+                    <h1 className="title-font text-lg font-medium text-black mb-3">Twitter</h1>
+                    <p className="leading-relaxed mb-3">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                    </p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-blue-400 group-hover:from-cyan-500 inline-flex items-center md:mb-2 lg:mb-0" href="#">
+                        Check Out
+                        <svg
+                          className="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 md:w-1/3">
+                <div className="h-full border-2 border-slate-300 rounded-lg overflow-hidden">
+                  <Image
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src="/img/Screenshot 2024-04-10 154642.png"
+                    alt="Twitter Clone"
+                    width={500}
+                    height={300}
+                  />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Clone</h2>
+                    <h1 className="title-font text-lg font-medium text-black mb-3">Twitter</h1>
+                    <p className="leading-relaxed mb-3">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                    </p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-blue-400 group-hover:from-cyan-500 inline-flex items-center md:mb-2 lg:mb-0" href="#">
+                        Check Out
+                        <svg
+                          className="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 md:w-1/3">
+                <div className="h-full border-2 border-slate-300 rounded-lg overflow-hidden">
+                  <Image
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src="/img/Screenshot 2024-04-10 154642.png"
+                    alt="Twitter Clone"
+                    width={500}
+                    height={300}
+                  />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Clone</h2>
+                    <h1 className="title-font text-lg font-medium text-black mb-3">Twitter</h1>
+                    <p className="leading-relaxed mb-3">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                    </p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-blue-400 group-hover:from-cyan-500 inline-flex items-center md:mb-2 lg:mb-0" href="#">
+                        Check Out
+                        <svg
+                          className="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 md:w-1/3">
+                <div className="h-full border-2 border-slate-300 rounded-lg overflow-hidden">
+                  <Image
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src="/img/Screenshot 2024-04-10 154642.png"
+                    alt="Twitter Clone"
+                    width={500}
+                    height={300}
+                  />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Clone</h2>
+                    <h1 className="title-font text-lg font-medium text-black mb-3">Twitter</h1>
+                    <p className="leading-relaxed mb-3">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                    </p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-blue-400 group-hover:from-cyan-500 inline-flex items-center md:mb-2 lg:mb-0" href="#">
+                        Check Out
+                        <svg
+                          className="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 md:w-1/3">
+                <div className="h-full border-2 border-slate-300 rounded-lg overflow-hidden">
+                  <Image
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src="/img/Screenshot 2024-04-10 154642.png"
+                    alt="Twitter Clone"
+                    width={500}
+                    height={300}
+                  />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Clone</h2>
+                    <h1 className="title-font text-lg font-medium text-black mb-3">Twitter</h1>
+                    <p className="leading-relaxed mb-3">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                    </p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-blue-400 group-hover:from-cyan-500 inline-flex items-center md:mb-2 lg:mb-0" href="#">
+                        Check Out
+                        <svg
+                          className="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </section>
+      </div>
 
     </>
   );
---
+++ Level 1/portfolio/components/LanguageProficiency.js
@@ -0,0 +1,44 @@
+// components/LanguageProficiency.js
+import Image from 'next/image';
+
+const LanguageProficiency = () => {
+  const languages = [
+    { name: 'Python', proficiency: 40, img: '/img/python.svg' },
+    { name: 'HTML', proficiency: 75, img: '/img/file-type-html.svg' },
+    { name: 'CSS', proficiency: 70, img: '/img/file-type-css.svg' },
+    { name: 'JavaScript', proficiency: 45, img: '/img/file-type-js-official.svg' },
+    { name: 'C', proficiency: 60, img: '/img/c-original.svg' },
+    { name: 'C++', proficiency: 35, img: '/img/cpp.svg' },
+    { name: 'SQL', proficiency: 40, img: '/img/mysql-original-wordmark.svg' },
+    { name: 'MongoDB', proficiency: 35, img: '/img/mongodb.svg' },
+  ];
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="sm:text-3xl text-3xl font-medium text-center title-font text-black mb-8">
+        Languages Known
+      </h1>
+      <div className="flex flex-wrap -m-2">
+        {languages.map((lang) => (
+          <div key={lang.name} className="p-2 sm:w-1/2 w-full">
+            <div className="bg-slate-300 rounded flex p-4 h-full items-center justify-between transition-transform duration-500 transform hover:scale-105">
+              <Image src={lang.img} alt={lang.name} width={60} height={60} className="transition-transform duration-500 transform hover:scale-125" />
+              <span className="title-font font-medium text-lg text-black ml-4">{lang.name}</span>
+              <div className="w-2/3 bg-gray-200 rounded-full h-4 ml-4">
+                <div
+                
+                  className="bg-gradient-to-br from-cyan-300 to-blue-400 group-hover:from-cyan-500 h-4 rounded-full transition-all duration-500 ease-in-out"
+                  style={{ width: `${lang.proficiency}%` }}
+                ></div>
+              </div>
+              <span className="ml-4 text-lg text-black">{lang.proficiency}%</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default LanguageProficiency;
